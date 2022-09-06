dc34aaa638e830a4855cf42809d606fb8f9089bc
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-06 08:09:41 -0300

    Add CartViewController

--- GrowMartApp/Components/CategoryButton.swift
+++ GrowMartApp/Components/CategoryButton.swift
@@ -14,6 +14,7 @@ class CategoryButton: UIControl {
         case right
     }
 
+    let categoryId: Int
     private let title: String
     private let imageSide: ImageSide
     private let image: UIImage?
@@ -37,7 +38,8 @@ class CategoryButton: UIControl {
     }()
 
     // MARK: - Inits
-    internal init(title: String, imageSide: CategoryButton.ImageSide, image: UIImage?) {
+    internal init(categoryId: Int, title: String, imageSide: CategoryButton.ImageSide, image: UIImage?) {
+        self.categoryId = categoryId
         self.title = title
         self.imageSide = imageSide
         self.image = image
