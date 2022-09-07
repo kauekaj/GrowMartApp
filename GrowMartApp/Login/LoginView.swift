644bfede21e77b7922dfd40f24ec98bc9cd31efa
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-07 08:10:50 -0300

    Update LoginView

--- GrowMartApp/Login/LoginView.swift
+++ GrowMartApp/Login/LoginView.swift
@@ -44,44 +44,32 @@ class LoginView: UIView {
     }()
 
     private lazy var facebookLoginButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 8
-        button.addTarget(self, action: #selector(didTapFacebookLogin), for: .touchUpInside)
-        button.configuration = getButtonConfigurations(backgroundColor: .init(rgb: 0x4267B2),
-                                                       title: "ENTRAR COM FACEBOOK",
-                                                       icon: UIImage(named: "facebook-logo"))
-        return button
-    }()
+            let element = UIButton()
+            element.translatesAutoresizingMaskIntoConstraints = false
+            element.setTitleColor(.white, for: .normal)
+            element.layer.cornerRadius = 8
+            element.addTarget(self, action: #selector(didTapFacebookLogin), for: .touchUpInside)
+            element.configuration = .makeWith(backgroundColor: .init(rgb: 0x4267B2),
+                                              title: "ENTRAR COM FACEBOOK",
+                                              font: .nunito(style: .semiBold, size: 12),
+                                              icon: UIImage(named: "facebook-logo"))
+            return element
+        }()
 
-    private lazy var googleLoginButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 8
-        button.setImage(UIImage(named: "google-logo"), for: .normal)
-        button.addTarget(self, action: #selector(didTapGoogleLogin), for: .touchUpInside)
-        button.configuration = getButtonConfigurations(backgroundColor: .init(rgb: 0xDB4437),
-                                                       title: "ENTRAR COM GOOGLE",
-                                                       icon: UIImage(named: "google-logo"))
-        return button
-    }()
+        private lazy var googleLoginButton: UIButton = {
+            let element = UIButton()
+            element.translatesAutoresizingMaskIntoConstraints = false
+            element.setTitleColor(.white, for: .normal)
+            element.layer.cornerRadius = 8
+            element.addTarget(self, action: #selector(didTapGoogleLogin), for: .touchUpInside)
+            element.configuration = .makeWith(backgroundColor: .init(rgb: 0xDB4437),
+                                              title: "ENTRAR COM GOOGLE",
+                                              font: .nunito(style: .semiBold, size: 12),
+                                              icon: UIImage(named: "google-logo"))
+            return element
+        }()
 
     // MARK: - Private Methods
-    private func getButtonConfigurations(backgroundColor: UIColor,
-                                         title: String,
-                                         icon: UIImage?) -> UIButton.Configuration {
-        var configuration = UIButton.Configuration.filled()
-        configuration.title = title
-        configuration.attributedTitle = AttributedString(title, attributes: getFontAttributes())
-        configuration.image = icon
-        configuration.titlePadding = 10
-        configuration.imagePadding = 30
-        configuration.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
-        configuration.background.backgroundColor = backgroundColor
-        return configuration
-    }
 
     // MARK: - Inits
     override init(frame: CGRect) {
@@ -92,10 +80,6 @@ class LoginView: UIView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
-    }
-
-    private func getFontAttributes() -> AttributeContainer {
-        AttributeContainer([NSAttributedString.Key.font : UIFont.nunito(style: .semiBold, size: 12)])
     }
 
     // MARK: - Actions
