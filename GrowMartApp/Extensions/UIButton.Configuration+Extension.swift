e959986c311c50b117d3d8519f6e32c5e15b31a2
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-07 08:06:12 -0300

    Create UIButton.Configuration Extension

---
+++ GrowMartApp/Extensions/UIButton.Configuration+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIButton.Configuration+Extension.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 07/09/22.
+//
+
+import UIKit
+
+extension UIButton.Configuration {
+    public static func makeWith(backgroundColor: UIColor,
+                                title: String,
+                                font: UIFont,
+                                icon: UIImage? = nil) -> UIButton.Configuration {
+        var configuration = UIButton.Configuration.filled()
+        let attributeContainer = AttributeContainer([NSAttributedString.Key.font : font])
+        configuration.attributedTitle = AttributedString(title, attributes: attributeContainer)
+        configuration.image = icon
+        configuration.titlePadding = 10
+        configuration.imagePadding = 30
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+        configuration.background.backgroundColor = backgroundColor
+        return configuration
+    }
+}
