2ff90c5b6c7809c2d5d73a7cd10f2d13a9ab8b74
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-11 20:34:21 -0300

    Update CatalogView

---
+++ GrowMartApp/Modules/Catalog/CatalogView.swift
@@ -0,0 +1,25 @@
+//
+//  CatalogView.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 10/09/22.
+//
+
+import UIKit
+
+public protocol CatalogViewDelegate: AnyObject {
+
+}
+
+class CatalogView: UIView {
+
+    // MARK: - Public Properties
+    weak var delegate: CatalogViewDelegate?
+    
+    // MARK: - Private Properties
+
+    
+    // MARK: - Inits
+
+    
+}
