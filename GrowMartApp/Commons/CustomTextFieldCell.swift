6cb71177e3d4b1fd18594744bc421d9bee851640
Kaue Jacyntho <kauekaj@gmail.com>
2022-09-24 12:06:00 -0300

    Create Common folder to add all custom Cells

---
+++ GrowMartApp/Commons/CustomTextFieldCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomTextFieldCell.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 24/09/22.
+//
+
+import UIKit
+
+public final class CustomTextFieldCell: UITableViewCell {
+    
+}
