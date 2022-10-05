b52bbff8f99423284493ea6ce4b12cf8f18469d2
Kaue Jacyntho <kauekaj@gmail.com>
2022-10-05 06:42:24 -0300

    Configured BaseTests

---
+++ GrowMartAppTests/BaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaseTests.swift
+//  GrowMartAppTests
+//
+//  Created by Kaue de Assis Jacyntho on 04/10/22.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import GrowMartApp
+
+class BaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+//        SnapshotTesting.isRecording = true
+    }
+}
