8fc105d9536b399a82588f1984673e1c5d277e0b
Kaue Jacyntho <kauekaj@gmail.com>
2022-10-05 07:21:59 -0300

    Implemented CategoriesSegmentedControl Snapshot tests

---
+++ GrowMartAppTests/Components/CategoriesSegmentedControlTests.swift
@@ -0,0 +1,33 @@
+//
+//  CategoriesSegmentedControlTests.swift
+//  GrowMartAppTests
+//
+//  Created by Kaue de Assis Jacyntho on 05/10/22.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import GrowMartApp
+
+class CategoriesSegmentedControlTests: BaseTests {
+    private let componentSize = CGSize(width: 414, height: 45)
+    
+    override func setUp() {
+//        SnapshotTesting.isRecording = true
+    }
+  
+    func testRenderSegmentedWithTwoElements() {
+        let component = CategoriesSegmentedControl(items: ["roupas", "acessórios"])
+        assertSnapshot(matching: component, as: .image(size: componentSize))
+    }
+    
+    func testRenderSegmentedWithThreeElements() {
+        let component = CategoriesSegmentedControl(items: ["roupas", "acessórios", "outros"])
+        assertSnapshot(matching: component, as: .image(size: componentSize))
+    }
+    
+    func testRenderSegmentedWithFourElements() {
+        let component = CategoriesSegmentedControl(items: ["primeiro", "segundo", "terceiro", "quarto"])
+        assertSnapshot(matching: component, as: .image(size: componentSize))
+    }
+}
