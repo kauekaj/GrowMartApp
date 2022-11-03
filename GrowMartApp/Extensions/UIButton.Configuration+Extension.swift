c6691cff40ba2bacd340a147c3af7977a743721e
Kaue Jacyntho <kauekaj@gmail.com>
2022-11-03 08:09:39 -0300

    Update UIButton Configuration - add imagePlacement parameter

--- GrowMartApp/Extensions/UIButton.Configuration+Extension.swift
+++ GrowMartApp/Extensions/UIButton.Configuration+Extension.swift
@@ -11,13 +11,15 @@ extension UIButton.Configuration {
     public static func makeWith(backgroundColor: UIColor,
                                 title: String = "",
                                 font: UIFont,
-                                icon: UIImage? = nil) -> UIButton.Configuration {
+                                icon: UIImage? = nil,
+                                imagePlacement: NSDirectionalRectEdge = .leading) -> UIButton.Configuration {
         var configuration = UIButton.Configuration.filled()
-        let attributeContainer = AttributeContainer([NSAttributedString.Key.font : font])
+        let attributeContainer: AttributeContainer = .init([NSAttributedString.Key.font : font])
         configuration.attributedTitle = AttributedString(title, attributes: attributeContainer)
         configuration.image = icon
         configuration.titlePadding = 10
         configuration.imagePadding = 30
+        configuration.imagePlacement = imagePlacement
         configuration.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
         configuration.background.backgroundColor = backgroundColor
         return configuration
