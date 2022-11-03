b20992f65167c8432df3b32e0ecb147aced83e8a
Kaue Jacyntho <kauekaj@gmail.com>
2022-11-03 08:05:55 -0300

    Created FavoriteButton

---
+++ GrowMartApp/Components/Buttons/FavoriteButton.swift
@@ -0,0 +1,32 @@
+//
+//  FavoriteButton.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 03/11/22.
+//
+
+import UIKit
+
+class FavoriteButton: UIButton {
+    var productId = ""
+    var isFavorite = false {
+        didSet {
+            let color: UIColor = isFavorite ? .init(rgb: 0xFFC13B) : .white
+            setImage(.makeWith(systemImage: .starFill, color: color), for: .normal)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setImage(.makeWith(systemImage: .starFill, color: .white), for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func toggleState() {
+        isFavorite = !isFavorite
+    }
+}
