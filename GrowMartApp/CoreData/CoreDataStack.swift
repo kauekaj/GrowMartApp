81649cf30d72b5eee0a7f1482608aed3cb97a5e4
Kaue Jacyntho <kauekaj@gmail.com>
2022-11-04 07:11:33 -0300

    Add CoreData - Create Favorite

---
+++ GrowMartApp/CoreData/CoreDataStack.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataStack.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 04/11/22.
+//
+
+// Source: https://johncodeos.com/how-to-use-core-data-in-ios-using-swift/
+import CoreData
+
+class CoreDataStack {
+    private let modelName: String
+
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    lazy var managedContext: NSManagedObjectContext = self.storeContainer.viewContext
+
+    func saveContext() {
+        guard managedContext.hasChanges else { return }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
