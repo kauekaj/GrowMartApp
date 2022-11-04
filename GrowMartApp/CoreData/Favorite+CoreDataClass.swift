5b5fd8c1047c29893cd3b8e0e9c3fc45018fc744
Kaue Jacyntho <kauekaj@gmail.com>
2022-11-04 07:17:22 -0300

    Created Favorite Class with CoreDataProperties

---
+++ GrowMartApp/CoreData/Favorite+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Favorite+CoreDataClass.swift
+//  
+//
+//  Created by Kaue de Assis Jacyntho on 04/11/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Favorite)
+public class Favorite: NSManagedObject {
+
+}
---
+++ GrowMartApp/CoreData/Favorite+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Favorite+CoreDataProperties.swift
+//  
+//
+//  Created by Kaue de Assis Jacyntho on 04/11/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Favorite {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Favorite> {
+        return NSFetchRequest<Favorite>(entityName: "Favorite")
+    }
+
+    @NSManaged public var identifier: String?
+    @NSManaged public var image: String?
+    @NSManaged public var name: String?
+    @NSManaged public var price: String?
+
+}
