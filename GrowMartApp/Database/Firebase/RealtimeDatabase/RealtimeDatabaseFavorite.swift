ab0a9ba797b9a1e1a5979774310b25b84ef70821
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-02 13:46:24 -0300

    Create RealtimeDatabaseFavorite

---
+++ GrowMartApp/Database/Firebase/RealtimeDatabase/RealtimeDatabaseFavorite.swift
@@ -0,0 +1,24 @@
+//
+//  RealtimeDatabaseFavorite.swift
+//  GrowMartApp
+//
+//  Created by Kaue de Assis Jacyntho on 02/12/22.
+//
+
+class RealtimeDatabaseFavorite {
+    var identifier: String = ""
+    var image: String = ""
+    var name: String = ""
+    var price: String = ""
+
+    convenience init(identifier: String?,
+                     image: String?,
+                     name: String?,
+                     price: String?) {
+           self.init()
+           self.identifier = identifier ?? ""
+           self.image = image ?? ""
+           self.name = name ?? ""
+           self.price = price ?? ""
+       }
+}
