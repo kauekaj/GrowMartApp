c6374a9e46013a06b968f1dd60264bc347d695d8
Kaue Jacyntho <kauekaj@gmail.com>
2022-12-04 18:05:51 -0300

    Update RealtimeDatabasefavorite with toDictionary func

--- GrowMartApp/Database/Firebase/RealtimeDatabase/RealtimeDatabaseFavorite.swift
+++ GrowMartApp/Database/Firebase/RealtimeDatabase/RealtimeDatabaseFavorite.swift
@@ -10,15 +10,24 @@ class RealtimeDatabaseFavorite {
     var image: String = ""
     var name: String = ""
     var price: String = ""
-
+    
     convenience init(identifier: String?,
                      image: String?,
                      name: String?,
                      price: String?) {
-           self.init()
-           self.identifier = identifier ?? ""
-           self.image = image ?? ""
-           self.name = name ?? ""
-           self.price = price ?? ""
-       }
+        self.init()
+        self.identifier = identifier ?? ""
+        self.image = image ?? ""
+        self.name = name ?? ""
+        self.price = price ?? ""
+    }
+    
+    func toDictionary() -> [String: String] {
+        [
+            "identifier":identifier,
+            "image":image,
+            "name": name,
+            "price": price
+        ]
+    }
 }
