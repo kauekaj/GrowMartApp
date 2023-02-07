1efcfcea6ea9eeaaf854e4019a09a87f573e7840
Kaue Jacyntho <kauekaj@gmail.com>
2023-02-07 16:29:56 -0300

    * Add SwiftGen * Create Localizable

--- GrowMartApp/Database/CoreData/CoreDataFavorite+CoreDataProperties.swift
+++ GrowMartApp/Database/CoreData/CoreDataFavorite+CoreDataProperties.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CoreData
 
-
 extension CoreDataFavorite {
 
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CoreDataFavorite> {
--- GrowMartApp/Extensions/UIImage+Extensions.swift
+++ GrowMartApp/Extensions/UIImage+Extensions.swift
@@ -9,27 +9,27 @@ import UIKit
 
 extension UIImage {
     enum SystemImage: String {
-    case starFill = "star.fill"
-    case trashFill = "trash.fill"
-    case sliderHorizontal3 = "slider.horizontal.3"
-    case xmark = "xmark"
+        case starFill = "star.fill"
+        case trashFill = "trash.fill"
+        case sliderHorizontal3 = "slider.horizontal.3"
+        case xmark = "xmark"
     }
     
     static func makeWith(systemImage: SystemImage,
                          color: UIColor? = nil) -> UIImage? {
         var image = UIImage(systemName: systemImage.rawValue)
-
+        
         if let color = color {
             image = image?.withTintColor(color, renderingMode: .alwaysOriginal)
         }
         
         return image
     }
-
+    
     static func makeWith(name: String,
                          color: UIColor? = nil) -> UIImage? {
         var image = UIImage(named: name)
-
+        
         if let color = color {
             image = image?.withTintColor(color, renderingMode: .alwaysOriginal)
         }
